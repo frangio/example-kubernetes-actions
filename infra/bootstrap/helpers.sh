01070867e9417375c6824a6df4db1e9f37a166fc
Francisco Giordano <frangio.1@gmail.com>
2020-04-04 20:14:44 -0300

    fix output silencing

--- infra/bootstrap/helpers.sh
+++ infra/bootstrap/helpers.sh
@@ -1,4 +1,4 @@
 flux-install() {
-  kubectl get ns flux 2>&1 > /dev/null || kubectl create ns flux
+  kubectl get ns flux >/dev/null 2>&1 || kubectl create ns flux
   helm install -n flux --repo https://charts.fluxcd.io "$@"
 }
