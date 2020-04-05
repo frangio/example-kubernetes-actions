343dcff6ea6aac4787e65981caf169d4771309bf
Francisco Giordano <frangio.1@gmail.com>
2020-04-05 12:15:01 -0300

    remove unused bootstrap scripts

--- infra/bootstrap/helm-operator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "$BASH_SOURCE")"
-
-source helpers.sh
-
-flux-install helm-operator helm-operator --set helm.versions=v3
--- infra/bootstrap/helpers.sh
+++
@@ -1,4 +0,0 @@
-flux-install() {
-  kubectl get ns flux >/dev/null 2>&1 || kubectl create ns flux
-  helm install -n flux --repo https://charts.fluxcd.io "$@"
-}
